61352f35af8e1f90553cf03737695e0a4a407c5d
yahavb <ybiran@colostate.edu>
2016-03-17 18:01:45 +0000

    unlimited memory allocation to load.php

--- load.php
+++ load.php
@@ -1,4 +1,6 @@
 <?php
+// Allocate memory per the VM capacity
+ini_set('memory_limit', '-1');
 // Get a file into an array.  In this example we'll go through HTTP to get
 // the HTML source of a URL.
 $lines = file('1.log');
@@ -9,5 +11,5 @@ sort($lines);
 foreach ($lines as $line_num => $line) {
     echo "Line {$line_num} : $line\n";
 }*/
-
+echo "Done";
 ?>
